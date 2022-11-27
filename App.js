08196bc5bc4488d48352f9d6a644711c7609da16
DheVam <vanshidheeraj07@gmail.com>
2022-11-27 21:21:56 +0530

    Zoom Feauture

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import ImageViewer from 'react-native-image-zoom-viewer';
 
+
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+
+const images = [
+  { url: 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/sample_img.png' }
+]
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: windowWidth,
+      }}>
+        <ImageViewer imageUrls={images} style={{ width: 300, height: 300 }} footerContainerStyle={{ height: 20 }} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +35,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageDiv: {
+    height: windowHeight,
+    width: "100%",
+  },
+
 });
